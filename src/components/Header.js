d9fadbf2e47973472cf64dbba506ceb1634ece34
David DeRooy <dderooy@MacBook-Pro.local>
2019-03-30 21:15:34 -0400

    fixing lint errors and formatting

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,67 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Header = (props) => (
-    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
-        <div className="logo">
-            <span className="icon fa-code"></span>
-        </div>
-        <div className="content">
-            <div className="inner">
-                <h1>David DeRooy</h1>
-                <p> An engineer who enjoys programming and learning new things</p>
-            </div>
-        </div>
-        <nav>
-            <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('hobby')}}>Hobbies</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
-            </ul>
-        </nav>
-    </header>
+const Header = props => (
+  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+    <div className="logo">
+      <span className="icon fa-code" />
+    </div>
+    <div className="content">
+      <div className="inner">
+        <h1>David DeRooy</h1>
+        <p> An engineer who enjoys programming and learning new things</p>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <a
+            href="#/"
+            onClick={() => {
+              props.onOpenArticle('work')
+            }}
+          >
+            Work
+          </a>
+        </li>
+        <li>
+          <a
+            href="#/"
+            onClick={() => {
+              props.onOpenArticle('about')
+            }}
+          >
+            About
+          </a>
+        </li>
+        <li>
+          <a
+            href="#/"
+            onClick={() => {
+              props.onOpenArticle('hobby')
+            }}
+          >
+            Hobbies
+          </a>
+        </li>
+        <li>
+          <a
+            href="#/"
+            onClick={() => {
+              props.onOpenArticle('contact')
+            }}
+          >
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </header>
 )
 
 Header.propTypes = {
-    onOpenArticle: PropTypes.func,
-    timeout: PropTypes.bool
+  onOpenArticle: PropTypes.func,
+  timeout: PropTypes.bool,
 }
 
 export default Header
